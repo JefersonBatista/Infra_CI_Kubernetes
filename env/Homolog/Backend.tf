b3a243c1108095f89ba1ddb9bb1bea08dc785f73
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2024-10-29 15:22:36 -0300

    chore: rename s3 bucket

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-jefersonbatista"
+    bucket = "tf-jefersonbatista-state"
     key    = "Homolog/terraform.tfstate"
     region = "us-east-1"
   }
